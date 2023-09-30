01daa2f2559d9699cf1e826092b6734477d885f9
BhavyaKushwaha <138321349+BhavyaKushwaha@users.noreply.github.com>
2023-09-30 11:31:08 +0530

    Update VoterID.py

--- VoterID.py
+++ VoterID.py
@@ -1,6 +1,6 @@
 # The following code has some errors, students are encouraged to correct them and push the changes.
-age = int(input("Enter you Legal age:"))
-if(age > 18)
+age = int(input("Enter your Legal age:"))
+if(age > 18):
   print("You are eligible to vote!")
-else
+else:
   print("You are underage to vote right now!")
